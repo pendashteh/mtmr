34468b3e4890643d6efb34e50020a3e20e719e52
Alexar <github@alexar.me>
2020-06-08 23:19:19 +1000

    Provides more information and better formatting for the output.

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,18 +1,69 @@
 'use strict';
 
-let page = document.getElementById('page');
-
-function print_tabs() {
+function get_tabs(callback) {
   chrome.tabs.query({}, function(tabs) {
-    chrome.storage.sync.set({tabs: tabs});
-    let tabnames = [];
+    // chrome.storage.sync.set({tabs: JSON.stringify(tabs)});
+    let tabsinfo = {};
     tabs.forEach(tab => {
-      tabnames.push(`<b>\n${tab.title}\n</b><br>\n<a href="${tab.url}">\n${tab.url}\n</a>\n`);
+      tabsinfo[tab.windowId] = tabsinfo[tab.windowId] || {tabs:[]};
+      tabsinfo[tab.windowId].tabs.push(tab)
     });
-    let p = document.createElement('p');
-    p.innerHTML = tabnames.join('<hr>');
-    page.appendChild(p);
+    callback(tabsinfo)
   })
 }
 
-print_tabs();
+class output {
+  constructor(html) {
+    this.p = document.createElement('p');
+  }
+  add(html) {
+    this.p.innerHTML += (html||'') + "\n";
+  }
+  done() {
+    this.p.innerHTML = `<xmp>${this.p.innerHTML}</xmp>`;
+    document.getElementById('page').appendChild(this.p);
+  }
+}
+
+function format_json(tabsinfo) {
+    let o = new output();
+    o.add(JSON.stringify(tabsinfo));
+    o.done();
+    return
+}
+
+function format_html(tabsinfo) {
+  let o = new output();
+  for (let winId in tabsinfo) {
+    o.add('# Window #' + winId);
+    for (let tab of tabsinfo[winId].tabs) {
+      let status = [];
+      let search_states = ['highlighted', 'active', 'pinned', 'audible', 'discarded'];
+      for (let state of search_states) {
+        tab[state] && status.push(state);
+      }
+      tab.openerTabId && status.push(`opened by #${tab.openerTabId}`);
+      console.log(status)
+      o.add(`## ${tab.index+1}. Tab #${tab.id} -- ${status.join(',')}`);
+      o.add(`${tab.title}`);
+      o.add(`${tab.url}`);
+      for (let key of ['sessionId']) {
+        o.add(tab[key]);
+      }
+      o.add(JSON.stringify(tab, function(key, value) {
+        if (search_states.includes(key)) {
+          return undefined;
+        }
+        if (key == 'url') {
+          return undefined;
+        }
+        return value
+      }));
+      o.add();
+
+    }
+  }
+  o.done()
+}
+
+get_tabs(format_html);
