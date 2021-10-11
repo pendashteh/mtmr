c83bd26935b894de53adf19ad7e4beee5f5b5eb0
Alexar <github@alexar.me>
2021-10-11 13:53:28 +1100

    Provides multiple formatings.

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -8,15 +8,22 @@ function get_tabs(callback) {
 }
 
 class output {
-  constructor(html) {
+  constructor(clear=true) {
     this.p = document.createElement('p');
+    this.body = document.getElementById('page');
+    if (clear) {
+      this.clear()
+    }
+  }
+  clear() {
+    this.body.innerHTML = '';
   }
   add(html) {
     this.p.innerHTML += (html||'') + "\n";
   }
   done() {
     this.p.innerHTML = `<xmp>${this.p.innerHTML}</xmp>`;
-    document.getElementById('page').appendChild(this.p);
+    this.body.appendChild(this.p);
   }
 }
 
@@ -54,4 +61,57 @@ function format_md(tabs) {
   o.done();
 }
 
-get_tabs(format_md);
+
+function format_groups(tabs) {
+  let o = new output();
+  let urls=[];
+  let currentWindowId=0;
+  o.add(`Time: ${new Date().toLocaleString()}`);
+  for (let tab of tabs) {
+    if (currentWindowId != tab.windowId) {
+      o.add()
+      o.add(`Window #${tab.windowId}:`);
+      currentWindowId = tab.windowId;
+      o.add()
+    }
+    o.add(`[${tab.title}][#${tab.id}]`);
+    o.add(`[#${tab.id}]: ${tab.url}`);
+    // o.add(`${tab.title}`);
+    // o.add(`${tab.url}`);
+    o.add()
+  }
+  o.done();
+}
+
+function page_call(page) {
+  switch (page) {
+    case 'main':
+      // @TODO populate windows in select
+    break;
+    case 'groups':
+      get_tabs(format_groups);
+    break;
+    case 'export':
+      get_tabs(format_md);
+    break;
+    case 'json':
+      get_tabs(format_json);
+    break;
+    default:
+  }
+}
+
+
+var anchor_click = function () {
+  document.getElementById('menu').style.display='none';
+  let page = this.getAttribute('data-page');
+  page_call(page)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('menu').style.display='block';
+  var items = document.getElementsByTagName("a");
+  for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', anchor_click);
+  }
+});
