a00e69b3f66eda18714985b547f61258f26b06d7
Alexar <github@alexar.me>
2021-11-15 14:16:15 +1100

    Adds a task: job. Regularily checks the given path (e.g. Downloads) for mtmr export to capture.

--- mtmr.sh
+++ mtmr.sh
@@ -30,5 +30,16 @@ move__ () {
   git commit -m'Updates the changes'
 }
 
+job__help='@arg src: path to dir for mtmr* files @does move mtmr* files to $datadir__'
+job__ () {
+  local src=$1
+  test -d "$src" || return -1
+  echo checking for $src/mtmr*
+  while true; do
+    find $src -name "mtmr*" -exec $__app__ --confirm-- off move {} \;
+    sleep 60
+  done
+}
+
 . undies
 
